345ae6ddf72d71012c977ea299141af64c13f5b7
Taaku0405 <rakosutaa@gmail.com>
2022-07-14 03:59:59 +0000

    修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,44 +2,44 @@ class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @address_date = ShippingAddress.new
-    @address_dates = current_customer.shipping_addresses
+    @address_date = Address.new
+    @address_dates = current_customer.addresses
 
   end
 
   def edit
-    @address_date = ShippingAddress.find(params[:id])
+    @address_date = Address.find(params[:id])
   end
 
   def create
-    @address_date = ShippingAddress.new(address_date_params)
+    @address_date = Address.new(address_date_params)
     @address_date.customer_id = current_customer.id
-    @address_dates = current_customer.shipping_addresses
+    @address_dates = current_customer.addresses
     if @address_date.save
       flash.now[:notice] = "配送先を登録しました。"
     end
   end
 
   def update
-    @address_date = ShippingAddress.find(params[:id])
+    @address_date = Address.find(params[:id])
     if @address_date.update(address_date_params)
-      redirect_to shipping_addresses_path, notice: "配送先を編集しました。"
+      redirect_to addresses_path, notice: "配送先を編集しました。"
     else
       render "edit"
     end
   end
 
   def destroy
-    @address_date = ShippingAddress.find(params[:id])
+    @address_date = Address.find(params[:id])
     @address_date.destroy
-    @address_dates = current_customer.shipping_addresses
+    @address_dates = current_customer.addresses
     flash.now[:notice] = "配送先を削除しました。"
   end
 
   private
 
   def address_date_params
-    params.require(:shipping_address).permit(:postal_code, :address, :name)
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 
 end
